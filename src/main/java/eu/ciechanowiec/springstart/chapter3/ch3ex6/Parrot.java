8745c335fb9ef2beb6a26ac47bb9c7cb641565fd
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-04-13 13:19:00 +0200

    Publish ch3ex6

---
+++ src/main/java/eu/ciechanowiec/springstart/chapter3/ch3ex6/Parrot.java
@@ -0,0 +1,17 @@
+package eu.ciechanowiec.springstart.chapter3.ch3ex6;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Herman Ciechanowiec
+ */
+@Component
+class Parrot {
+
+    private String name = "Koko";
+
+    @Override
+    public String toString() {
+        return "Parrot: " + name;
+    }
+}
---
+++ src/main/java/eu/ciechanowiec/springstart/chapter3/ch3ex6/Person.java
@@ -0,0 +1,27 @@
+package eu.ciechanowiec.springstart.chapter3.ch3ex6;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Herman Ciechanowiec
+ */
+@Component
+class Person {
+
+    private String name = "Ella";
+    private Parrot parrot;
+
+    @Autowired
+    public void setParrot(Parrot parrot) {
+        this.parrot = parrot;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+}
