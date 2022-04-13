ac3e963e4e3e9cb2e982a06feb9198a320ca0d8b
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-04-13 14:08:05 +0200

    Publish ch3ex9

---
+++ src/main/java/eu/ciechanowiec/springstart/chapter3/ch3ex9/Person.java
@@ -0,0 +1,26 @@
+package eu.ciechanowiec.springstart.chapter3.ch3ex9;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Herman Ciechanowiec
+ */
+@Component
+class Person {
+
+    private final String name = "Ella";
+
+    private final Parrot parrot;
+
+    Person(Parrot parrot2) {
+        this.parrot = parrot2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+}
--- src/main/java/eu/ciechanowiec/springstart/chapter3/ch3ex9/ProjConfig.java
+++ src/main/java/eu/ciechanowiec/springstart/chapter3/ch3ex9/ProjConfig.java
@@ -1,4 +1,4 @@
-package eu.ciechanowiec.springstart.chapter3.ch3ex8;
+package eu.ciechanowiec.springstart.chapter3.ch3ex9;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -13,25 +13,17 @@ import org.springframework.context.annotation.Configuration;
 class ProjConfig {
 
     @Bean
-    public Parrot parrot1() {
+    Parrot parrot1() {
         Parrot parrot = new Parrot();
         parrot.setName("Koko");
         return parrot;
     }
 
     @Bean
-    public Parrot parrot2() {
+    Parrot parrot2() {
         Parrot parrot = new Parrot();
         parrot.setName("Miki");
         return parrot;
-    }
-
-    @Bean
-    public Person person(@Qualifier("parrot2") Parrot parrot) {
-        Person person = new Person();
-        person.setName("Ella");
-        person.setParrot(parrot);
-        return person;
     }
 
 }
