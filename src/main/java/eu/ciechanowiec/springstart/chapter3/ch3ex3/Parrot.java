1d157b0c2789cd670efc498e5f9514b62669b416
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-04-13 09:20:40 +0200

    Publish ch4ex3

---
+++ src/main/java/eu/ciechanowiec/springstart/chapter3/ch3ex3/Parrot.java
@@ -0,0 +1,26 @@
+package eu.ciechanowiec.springstart.chapter3.ch3ex3;
+
+/**
+ * @author Herman Ciechanowiec
+ */
+class Parrot {
+
+    private String name;
+
+    Parrot() {
+        System.out.println("Parrot created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Parrot: " + name;
+    }
+}
---
+++ src/main/java/eu/ciechanowiec/springstart/chapter3/ch3ex3/Person.java
@@ -0,0 +1,26 @@
+package eu.ciechanowiec.springstart.chapter3.ch3ex3;
+
+/**
+ * @author Herman Ciechanowiec
+ */
+class Person {
+
+    private String name;
+    private Parrot parrot;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Parrot getParrot() {
+        return parrot;
+    }
+
+    public void setParrot(Parrot parrot) {
+        this.parrot = parrot;
+    }
+}
